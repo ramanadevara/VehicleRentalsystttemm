bb25844922f45147dfb40b98733de37252651cbc
vickydevara <43718608+vickydevara@users.noreply.github.com>
2018-10-05 13:09:59 +0530

    rrrr

--- app/src/main/java/com/example/abhin/vehiclerentalsystem/Register.java
+++ app/src/main/java/com/example/abhin/vehiclerentalsystem/Register.java
@@ -1,7 +1,19 @@
 package com.example.abhin.vehiclerentalsystem;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.content.Intent;
+        import android.support.v7.app.AlertDialog;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+
+        import com.android.volley.RequestQueue;
+        import com.android.volley.Response;
+        import com.android.volley.toolbox.Volley;
+
+        import org.json.JSONException;
+        import org.json.JSONObject;
 
 public class Register extends AppCompatActivity {
 
@@ -9,5 +21,47 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText etName = (EditText) findViewById(R.id.etname);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        final EditText etAge = (EditText) findViewById(R.id.etAge);
+        final Button bRegister = (Button)findViewById(R.id.bRegister);
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String name = etName.getText().toString();
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+                final int age = Integer.parseInt(etName.getText().toString());
+
+                Response.Listener<String> responseListener =new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success){
+                                Intent intent = new Intent(Register.this,login.class);
+                                Register.this.startActivity(intent);
+                            }else
+                            {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Register.this);
+                                builder.setMessage("Registration failed")
+                                        .setNegativeButton("Retry",null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                RegisterRequest registerRequest = new RegisterRequest(name,username,age,password,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(Register.this);
+                queue.add(registerRequest);
+            }
+        });
     }
 }
